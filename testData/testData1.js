9d148d82529caead368920bca0e35e39c053e842
RavitejaSrinivasan-K <rebelravi2002@gmail.com>
2026-04-29 11:02:19 +0530

    i created test1 file

---
+++ testData/testData1.js
@@ -0,0 +1,8 @@
+export const sauceData = {
+    username : "standard_user" , 
+    password : "secret_sauce" , 
+    productName : "Sauce Labs Onesie" ,
+    fName : "Raviteja" , 
+    lName : "K" , 
+    pinCode : "600093" 
+}
